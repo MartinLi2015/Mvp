f239f1a122311180adcdda4e6b4835909e33eb85
MartinLi2015 <1025236025@qq.com>
2017-05-18 00:10:08 +0800

    往主页加内容

---
+++ common/src/main/java/com/martin/common/view/recyclerview/animation/BaseAnimation.java
@@ -0,0 +1,10 @@
+package com.martin.common.view.recyclerview.animation;
+
+import android.animation.Animator;
+import android.view.View;
+
+public interface  BaseAnimation {
+
+    Animator[] getAnimators(View view);
+
+}
---
+++ common/src/main/java/com/martin/common/view/recyclerview/entity/MultiItemEntity.java
@@ -0,0 +1,7 @@
+package com.martin.common.view.recyclerview.entity;
+
+public interface MultiItemEntity {
+
+    int getItemType();
+
+}
